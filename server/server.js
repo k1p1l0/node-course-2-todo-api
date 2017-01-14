570bf726ea166c1cab72669728d112e607d98641
Kirill Kozak <s0ht@mail.ru>
2017-01-14 21:15:53 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
@@ -27,6 +28,26 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	let {id} = req.params;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id)
+		.then((todo) => {
+			if (!todo) {
+				res.status(404).send();
+			}
+
+			res.status(200).send({todo});
+		})
+		.catch((e) => {
+			res.status(400).send();
+		})
 });
 
 app.listen(3000, () => console.log('Started on port 3000'));
